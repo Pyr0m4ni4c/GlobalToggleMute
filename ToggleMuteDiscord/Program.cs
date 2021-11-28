17659eb3556d787928df8080f6f3cf675b097d40
Tobias Aleithe <taleithe@hotmail.de>
2021-11-28 17:15:08 +0100

    Added 'comp' mode when setting arbitrary amount of args

--- ToggleMuteDiscord/Program.cs
+++ ToggleMuteDiscord/Program.cs
@@ -1,18 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-#define SINGLEDEV
+﻿using NAudio.CoreAudioApi;
 
-using NAudio.CoreAudioApi;
+bool singleDev = args.Length == 0;
+bool muted;
 
-#if !SINGLEDEV
-var mmdevices = new MMDeviceEnumerator().EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active).Select(p => p.AudioEndpointVolume).ToList();
-var muted = mmdevices.Any(d => d.Mute);
-mmdevices.ForEach(d => d.Mute = !muted);
-mmdevices.ForEach(d => d.Dispose());
-#else
-var mmdevices = new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Capture, Role.Communications).AudioEndpointVolume;
-var muted = mmdevices.Mute;
-mmdevices.Mute = !muted; 
-mmdevices.Dispose();
-#endif
+if (singleDev)
+{
+    var volumes = new MMDeviceEnumerator().EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active)
+        .Select(p => p.AudioEndpointVolume).ToList();
+    muted = volumes.Any(d => d.Mute);
+    volumes.ForEach(d => d.Mute = !muted);
+    volumes.ForEach(d => d.Dispose());
+}
+else
+{
+    var volume = new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Capture, Role.Communications).AudioEndpointVolume;
+    muted = volume.Mute;
+    volume.Mute = !muted;
+    volume.Dispose();
+}
 
 new System.Media.SoundPlayer(!muted ? ToggleMuteDiscord.Properties.Resources.Mutesound : ToggleMuteDiscord.Properties.Resources.Unmutesound).PlaySync();
